7f58a092c8bf80b964ad9efd56b642948da30f0d
footballand22i <andreaskoch2222@gmail.com>
2018-10-19 17:05:11 +0200

    Debugging test

--- src/account/Test/UserTest.java
+++ src/account/Test/UserTest.java
@@ -1,10 +1,10 @@
 package account.Test;
 
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 
 import java.io.IOException;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 import common.Communication;
@@ -24,13 +24,13 @@ public class UserTest {
 
 		byte[] msgBytes = new byte[] { 0, 19, 116, 101, 115, 116, 64, 116, 111, 116, 97, 108, 108, 121, 110, 111, 116,
 				97, 102, 97, 107, 101, 109, 97, 105, 108, 46, 99, 111, 109, 19, 121, 97, 121, 97, 49, 50, 51, 52, 20 };
-		assertThat(registrationMsg.getMsgBytes(), CoreMatchers.is(msgBytes));
+		assertThat(registrationMsg.getMsgBytes(), is(msgBytes));
 
 		RegistrationMessage registrationMsgBack = new RegistrationMessage(registrationMsg.getMsgBytes());
 
-		assert (registrationMsgBack.getEmail().equals(email));
-		assert (registrationMsgBack.getPw().equals(pw));
-		assert (registrationMsgBack.getMessageType().equals(MESSAGETYPE.REGISTRATION_REQUEST));
+		assertThat(registrationMsgBack.getEmail(), is(email));
+		assertThat(registrationMsgBack.getPw(), is(pw));
+		assertThat(registrationMsgBack.getMessageType(), is(MESSAGETYPE.REGISTRATION_REQUEST));
 	}
 
 	@Test
@@ -45,34 +45,46 @@ public class UserTest {
 		System.out.println(registrationMsg.getMessageType());
 		System.out.println(registrationMsg.getEmail());
 		System.out.println(registrationMsg.getPw());
-		System.out.println(registrationMsg.getMsgBytes());		
+		System.out.println(registrationMsg.getMsgBytes());
 
-		(new Thread() {
-			@Override
-				public void run() {
-				ServerConnection server = new ServerConnection(port);
-			}
-		}).start();
-		
-		(new Thread() {
+		new Thread() {
 			@Override
 			public void run() {
+				System.out.println("Server started in thread " + Thread.currentThread().getId());
+				ServerConnection server = new ServerConnection(port);
+			}
+		}.start();
+
+		wait(500);
+		
+		new Thread() {
+			@Override
+			public void run() {
+				System.out.println("Client started in thread " + Thread.currentThread().getId());
 				RegistrationMessage registrationMsgBack = null;
 				try {
 					Communication communicator = new Communication(ip, port);
 					communicator.createSocket();
 					communicator.send(registrationMsg.getMsgBytes());
 					registrationMsgBack = new RegistrationMessage(communicator.receive());
-				} catch (IOException e) {
+					System.out.println(registrationMsgBack.getMessageType());
+					System.out.println(registrationMsgBack.getEmail());
+					System.out.println(registrationMsgBack.getPw());
+					System.out.println(registrationMsgBack.getMsgBytes());
+				} catch (Exception e) {
 					// TODO Auto-generated catch block
+					System.out.println("Error in thread " + Thread.currentThread().getId());
 					e.printStackTrace();
 				}
-				assert (registrationMsgBack.getMessageType().equals(MESSAGETYPE.REGISTRATION_SUCCESS));
-				assert (registrationMsgBack.getEmail().equals(email));
-				assert (registrationMsgBack.getPw().equals(pw));
+
+
+				System.out.println("Asserting in thread " + Thread.currentThread().getId());
+				assertThat(registrationMsgBack.getMessageType(), is(MESSAGETYPE.REGISTRATION_SUCCESS));
+				assertThat(registrationMsgBack.getEmail(), is(email));
+				assertThat(registrationMsgBack.getPw(), is(pw));
 			}
-	
-		}).start();
+
+		}.start();
 
 	}
 }
